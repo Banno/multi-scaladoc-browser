41ddff7dc7734a6f2fcce1ecf86219779bad7f72
Adam Shannon <adam.shannon@banno.com>
2015-02-19 11:51:04 -0600

    cleaning up imports

--- core/src/main/scala/net/virtualvoid/docs/Main.scala
+++ core/src/main/scala/net/virtualvoid/docs/Main.scala
@@ -1,25 +1,8 @@
 package net.virtualvoid.docs
-
-import akka.actor._
-import spray.can.Http
-import spray.routing.{ PathMatchers, HttpService }
-import spray.util.SprayActorLogging
-import akka.io.IO
-import java.io.{ InputStream, File }
-import java.util.zip.{ ZipEntry, ZipFile }
-import spray.json.{ JsValue, JsonParser, DefaultJsonProtocol, JsonFormat }
-import org.parboiled.common.FileUtils
-import spray.http.{ MediaTypes, StatusCodes, Uri }
-import spray.httpx.marshalling.{ Marshaller, BasicMarshallers }
-import spray.routing.directives.ContentTypeResolver
-import spray.http.StatusCodes.Redirection
-import spray.httpx.SprayJsonSupport
-import java.util.regex.Pattern
-import scala.util.matching.Regex.Match
+import akka.actor.ActorSystem
 
 object Main extends App {
   implicit val system = ActorSystem()
 
   MultiDocsBrowser.run(args.toSeq)
 }
-
